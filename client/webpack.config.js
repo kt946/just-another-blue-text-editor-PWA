d3562fd957f1ac041e80da4ecc3beb06fac96ab3
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-08 16:49:20 -0400

    added css loaders and babel to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,9 +51,29 @@ module.exports = () => {
       })
     ],
 
+    // CSS loaders and babel
     module: {
       rules: [
-        
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        }
       ],
     },
   };
