7013521b6732fe8a717813ff94dd62bb583be2b1
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-08 17:02:56 -0400

    added function to get all content from jate database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.error('putDb not implemented')
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the jate database');
+
+  // create connection to jate database and specify version
+  const jateDb = await openDB('jate', 1);
+
+  // create new transaction and specify store and data privlieges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // open up object store
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the jate database
+  const request = store.getAll();
+
+  // get confirmation of request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
