b7891b6729a6d25e6619e268ff0055e6a6725711
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-08 17:36:45 -0400

    updated event handlers for click and installed events

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,16 +5,16 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   butInstall.style.visibility = 'visible';
-
-  butInstall.addEventListener('click', () => {
-    event.prompt();
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed';
-  });
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', () => {
+  prompt();
+  butInstall.setAttribute('disabled', true);
+  butInstall.textContent = 'Installed';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
