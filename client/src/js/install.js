1202366adee518f558d15729521f63de6df0f400
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-10-03 23:17:57 -0400

    added comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,10 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  // stash the event so it can be triggered later
   window.deferredPrompt = event;
+  // display install button to user
   butInstall.classList.toggle('hidden', false);
 });
 
@@ -12,19 +15,22 @@ butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
+    // deferred prompt isn't available
     return;
   }
 
+  // show install prompt
   promptEvent.prompt();
 
+  // prompt() can only be called once
   window.deferredPrompt = null;
-
+  
+  // disable install button
   butInstall.setAttribute('disabled', true);
   butInstall.textContent = 'Installed';
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  window.deferredPrompt = null;
   console.log('👍', 'App has been installed!', event);
 });
